04308523641230588a06c5f8b905058e38ba2212
Saurabh Hindlekar <saurabh.hindlekar@hotmail.com>
2023-01-16 14:47:27 -0800

    step 7

--- backend/urls.py
+++ backend/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('products/', include('products.urls', namespace='products')),
     path('auth/', include('authtokens.urls', namespace="authtokens")),
+    path('users/', include('users.urls', namespace="users")),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+from users.serializers import CustomUserRegisterSerializer
+
+
+class CustomUserRegisterView(generics.CreateAPIView):
+    model = get_user_model()
+    serializer_class = CustomUserRegisterSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        return Response(status=status.HTTP_201_CREATED)
+
